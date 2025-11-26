b84275c35f925856dd496b410423e45af59429e9
krrishnacloud <krishnavemula423@gmail.com>
2025-11-26 19:39:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const app = express();
 app.use(express.static("public"));
 
 // API example (optional)
-app.get("/api/status", (req, res) => {
-  res.json({ status: "running" });
+app.get("/", (req, res) => {
+    res.send("OK");
 });
+
 
 
 // MUST match Dockerfile exposed port
