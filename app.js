513b4652c80cb84eaa7417aac3aec270d3738e7d
krrishnacloud <krishnavemula423@gmail.com>
2025-11-26 20:18:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/api/status", (req, res) => {
 // MUST match Dockerfile exposed port
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0' () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
